5ad0837a583772268d1eeef0ac940d446b17dd75
Ryan Poole <rmp135@gmail.com>
2018-01-03 20:46:55 +0000

    Export the module.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
 import Client from './Client'
-import RequestError from './RequestError'
+import {
+  ClientOptions,
+  DownloadPriority,
+  QueryTorrentFilter,
+  QueryTorrentsParameters,
+  FileDownloadOptions,
+  SharedDownloadOptions,
+  WebDownloadOptions
+} from './typings'
 
-import { DownloadPriority, ClientOptions } from './typings'
+export {
+  ClientOptions,
+  DownloadPriority,
+  QueryTorrentFilter,
+  QueryTorrentsParameters,
+  FileDownloadOptions,
+  SharedDownloadOptions,
+  WebDownloadOptions,
+  Client
+}
 
-;(async () => {
-  const options: ClientOptions = {
-    url: 'http://localhost:8080'
-  }
-  const client = new Client(options)
-  try {
-    const res = await client.setFilePriority('0e67a21331fcfe55e567cf2bbd208016a73e6361', 0, DownloadPriority.NORMAL)
-    console.log(JSON.stringify(res, null, 2))
-  } catch (error) {
-    if (error instanceof RequestError) {
-      console.log(error.status)
-    }
-    console.log(error.stack)
-  }
-})()
+export default Client
